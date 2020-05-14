14d99077b50b2afacc8e7be0253ae370d4e48021
Sebastien Lefevre <sebastien.lefevre05@hotmail.com>
2020-05-14 12:31:48 +0200

    new baseline

--- src/InterfaceEvent/AggregateEventI.java
+++
@@ -1,8 +0,0 @@
-package InterfaceEvent;
-
-import java.util.ArrayList;
-
-public interface AggregateEventI extends EventI {
-	ArrayList<EventI> getCorrelatedEvents();
-	
-}
--- src/InterfaceEvent/AtomicEventI.java
+++
@@ -1,8 +0,0 @@
-package InterfaceEvent;
-
-import java.io.Serializable;
-
-public interface AtomicEventI extends EventI{
-	Serializable putproperty(String name, Serializable value);
-	Serializable removeProperty(String name);
-}
