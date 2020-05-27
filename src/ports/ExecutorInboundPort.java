a0ba5851fb79d422fa38f5dc4a7ca28eb5f133ef
Hadrien Cazes <hadrien.cazes@gmail.com>
2020-05-27 16:07:55 +0200

    Renamed AlarmInboundPort to ExecutorInboundPort

---
+++ src/ports/ExecutorInboundPort.java
@@ -0,0 +1,30 @@
+package ports;	
+
+import fr.sorbonne_u.components.ComponentI;	
+import fr.sorbonne_u.components.ports.AbstractInboundPort;	
+import interfaces.executor.ExecutorCI;	
+import interfaces.executor.ExecutorCommandI;	
+
+public class ExecutorInboundPort  	
+extends AbstractInboundPort 	
+implements ExecutorCI{	
+
+	private static final long serialVersionUID = 1L;	
+
+	public ExecutorInboundPort(ComponentI owner) throws Exception {	
+		super (ExecutorCI.class, owner);	
+		assert	owner instanceof ExecutorCI ;	
+	}	
+
+	public ExecutorInboundPort( String uri,ComponentI owner) throws Exception {	
+		super (ExecutorCI.class, owner);	
+		assert	owner instanceof ExecutorCI ;	
+	}	
+
+	@Override	
+	public void execute(ExecutorCommandI command) {	
+		((ExecutorCI)this.owner).execute(command);	
+	}	
+
+
+}
