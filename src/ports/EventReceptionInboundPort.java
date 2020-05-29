0139f453c88450e9a0663415e25ea798229d3dd3
Sebastien Lefevre <sebastien.lefevre05@hotmail.com>
2020-05-29 09:48:42 +0200

    Changed getOwner Cast

--- src/ports/EventReceptionInboundPort.java
+++ src/ports/EventReceptionInboundPort.java
@@ -3,7 +3,6 @@ package ports;
 import components.CEPBus;
 import fr.sorbonne_u.components.ComponentI;
 import fr.sorbonne_u.components.ports.AbstractInboundPort;
-import interfaces.component.CEPBusManagementCI;
 import interfaces.component.EventReceptionCI;
 import interfaces.event.EventI;
 
@@ -19,14 +18,14 @@ implements EventReceptionCI
 	public EventReceptionInboundPort(String cepURI, ComponentI owner) 
 	throws Exception 
 	{
-		super (cepURI, CEPBus.class, owner);
+		super (cepURI, EventReceptionCI.class, owner);
 		assert owner instanceof CEPBus;
 	}
 
 	@Override
 	public void receiveEvent(String emitterURI, EventI e) throws Exception {
 		try {
-			((CEPBus) this.getOwner()).receiveEvent(emitterURI, e);
+			((EventReceptionCI) this.getOwner()).receiveEvent(emitterURI, e);
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
