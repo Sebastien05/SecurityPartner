a0be98540457ef15d1ab47e58398441cda9b5c03
Hadrien Cazes <hadrien.cazes@gmail.com>
2020-05-31 22:38:33 +0200

    correction for URI + added constant for future use in Energy...component

--- src/Events/EnergyConsumption.java
+++ src/Events/EnergyConsumption.java
@@ -4,7 +4,8 @@ import interfaces.event.AbstractAtomicEvent;
 
 public class EnergyConsumption extends AbstractAtomicEvent {
 
-	private static final String URI= "energyCunsumption";
+	private static final String URI= "energyConsumption";
+	public static final String ENERGY_PROPERTY = "Energy consumption";
 	private static int cpt = 0;
 	private int id;
 	
