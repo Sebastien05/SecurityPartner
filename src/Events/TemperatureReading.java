e8083827c02db442e195a40485a86d2414682436
Hadrien Cazes <hadrien.cazes@gmail.com>
2020-05-31 22:03:03 +0200

    fix rebase annotation for temperatureReading

--- src/Events/TemperatureReading.java
+++ src/Events/TemperatureReading.java
@@ -4,12 +4,9 @@ import interfaces.event.AbstractAtomicEvent;
 
 public class TemperatureReading extends AbstractAtomicEvent {
 
-<<<<<<< Upstream, based on master
 	private static final String URI= "temperatureReading";
-=======
-	private static final String URI_temperatureR= "temperatureReading";
 	public static final String TEMP_PROPERTY = "Current temperature";
->>>>>>> cdbb701 Added TEMP_PROPERTY constant TemperatureReading for easier use later
+	
 	private static int cpt = 0;
 	private int id;
 	
